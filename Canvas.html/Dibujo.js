915f38f59f0bf69514b31e5daa92916d9bae48ad
PaulinaAlvarez1 <pao199852@gmail.com>
2020-08-18 21:17:36 -0500

    canvas

--- Canvas.html/Dibujo.js
+++ Canvas.html/Dibujo.js
@@ -1,8 +1,19 @@
 var d = document.getElementById('dibujito');
 var lienzo = d.getContext('2d');
+var lineas = 50;
+var yi, xf;
+var colorcito = "#FAA";
 
-dibujarLinea("pink", 10, 300, 220, 10);
-dibujarLinea("red", 300, 10, 10, 200);
+for(l = 0; l < lineas; l++)
+{
+  yi = 10 * l;
+  xf = 10 * (l + 1);
+  dibujarLinea(colorcito, 300, yi, xf, 0);
+  console.log ("Linea" +l);
+}
+
+dibujarLinea(colorcito, 1, 1, 1, 299);
+dibujarLinea(colorcito, 1, 299, 299, 299);
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) 
 {
 lienzo.beginPath();
